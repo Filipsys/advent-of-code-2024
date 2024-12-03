ca1383d6005cfe9075c48e232eeced9e533cfa40
filyys <93247526+Filipsys@users.noreply.github.com>
2024-12-03 09:18:29 +0100

    day 3 task 1

---
+++ day-3/python/1.py
@@ -0,0 +1,14 @@
+import re
+
+
+data = open("../day-3.input", "r").read().replace("\n", "")
+
+regex_list = re.findall("mul\([0-9]{1,3},[0-9]{1,3}\)", data)
+
+result = 0
+for regex in regex_list:
+  left, right = regex[4:][:-1].split(",")
+  
+  result += int(left) * int(right)
+
+print(result)
