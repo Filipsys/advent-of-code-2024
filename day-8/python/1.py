ecd20ba9ac032add811ed5fcae10a630d676e1d6
filyys <93247526+Filipsys@users.noreply.github.com>
2024-12-10 13:58:11 +0100

    Update 1.py

--- day-8/python/1.py
+++ day-8/python/1.py
@@ -2,22 +2,56 @@ data = """..........
 ..........
 ..........
 ....a.....
-..........
+........a.
 .....a....
 ..........
 ..........
 ..........
-..........
-"""
+.........."""
 data = data.split("\n")
 
 possible_antenas = list("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-antenas_positions = []
+antennas_positions = []
 
 # Find all the antenas
 for line_index, line in enumerate(data):
   for char_index, char in enumerate(line):
     if char in possible_antenas:
-      antenas_positions.append((char_index, line_index))
+      antennas_positions.append((char_index, line_index))
 
-print(antenas_positions)
+print(antennas_positions)
+
+def get_distance_from_two_antenas(a, b):
+  return {
+    "x": abs(a[0] - b[0]), 
+    "y": abs(a[1] - b[1])
+  }
+
+
+
+# Temporarily turn the data into a list
+for index, line in enumerate(data):
+  data[index] = list(line)
+
+for index_antenna, antenna in enumerate(antennas_positions):
+  if index_antenna == len(antennas_positions) - 1:
+    continue
+
+  distances = get_distance_from_two_antenas(antennas_positions[index_antenna], antennas_positions[index_antenna + 1])
+  dist_x, dist_y = distances["x"], distances["y"]
+  print(dist_x, dist_y)
+
+  try:
+    data[antennas_positions[index_antenna][1] - dist_y][antennas_positions[index_antenna][0] - dist_x] = "#"
+  except IndexError:
+    print("Out of bounds")
+
+  try:
+    data[antennas_positions[index_antenna + 1][1] + dist_y][antennas_positions[index_antenna + 1][0] + dist_x] = "#"
+  except IndexError:
+    print("Out of bounds")
+
+
+
+for line in data:
+  print("".join(line))
