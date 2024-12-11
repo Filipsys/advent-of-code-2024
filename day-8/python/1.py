2549e3133234f6027805c9e3734b45c567f1aaa9
Filipsys <filipsysak@gmail.com>
2024-12-11 01:18:59 +0100

    Day 8 task 1 success

--- day-8/python/1.py
+++ day-8/python/1.py
@@ -1,57 +1,43 @@
-data = """..........
-..........
-..........
-....a.....
-........a.
-.....a....
-..........
-..........
-..........
-.........."""
+data = open("../day-8.input", "r").read()
 data = data.split("\n")
 
 possible_antenas = list("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 antennas_positions = []
+antinodes_positions = set()
+
+def get_distance_from_two_antenas(a, b):
+  return {
+    "x": a[0] - b[0], 
+    "y": a[1] - b[1]
+  }
 
 # Find all the antenas
 for line_index, line in enumerate(data):
   for char_index, char in enumerate(line):
     if char in possible_antenas:
-      antennas_positions.append((char_index, line_index))
-
-print(antennas_positions)
-
-def get_distance_from_two_antenas(a, b):
-  return {
-    "x": abs(a[0] - b[0]), 
-    "y": abs(a[1] - b[1])
-  }
-
-
+      antennas_positions.append((char_index, line_index, char))
 
 # Temporarily turn the data into a list
 for index, line in enumerate(data):
   data[index] = list(line)
 
-for index_antenna, antenna in enumerate(antennas_positions):
-  if index_antenna == len(antennas_positions) - 1:
-    continue
+for index_antenna, antenna in enumerate(antennas_positions):  
+  for index_other_antenna, other_antenna in enumerate(antennas_positions):
+    if index_antenna == index_other_antenna:
+      continue
 
-  distances = get_distance_from_two_antenas(antennas_positions[index_antenna], antennas_positions[index_antenna + 1])
-  dist_x, dist_y = distances["x"], distances["y"]
-  print(dist_x, dist_y)
-
-  try:
-    data[antennas_positions[index_antenna][1] - dist_y][antennas_positions[index_antenna][0] - dist_x] = "#"
-  except IndexError:
-    print("Out of bounds")
-
-  try:
-    data[antennas_positions[index_antenna + 1][1] + dist_y][antennas_positions[index_antenna + 1][0] + dist_x] = "#"
-  except IndexError:
-    print("Out of bounds")
-
-
-
-for line in data:
-  print("".join(line))
+    distances = get_distance_from_two_antenas(
+      antennas_positions[index_antenna], 
+      antennas_positions[index_other_antenna]
+      )
+    dist_x, dist_y = distances["x"], distances["y"]
+    new_x = other_antenna[0] - dist_x
+    new_y = other_antenna[1] - dist_y
+    
+    if 0 <= new_x < len(data[0]) and 0 <= new_y < len(data):
+      if antennas_positions[index_antenna][2] == antennas_positions[index_other_antenna][2]:
+        if (new_x, new_y) not in antinodes_positions:
+          antinodes_positions.add((new_x, new_y))
+    
+# Count the antinodes
+print("Antinodes amount: ", len(antinodes_positions))
