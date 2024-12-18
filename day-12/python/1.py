b777b7fdd6c78360096ae6d1c8deb15405862b0c
Filipsys <filipsysak@gmail.com>
2024-12-18 22:10:27 +0100

    I am tired please help me finish this

--- day-12/python/1.py
+++ day-12/python/1.py
@@ -5,7 +5,7 @@ EEEC"""
 data = data.split("\n")
 for index, line in enumerate(data):
   data[index] = list(line)
-
+  
 def get_surrounding_plants(plant_type):
   indicies = []
   for line_index, line in enumerate(data):
@@ -19,22 +19,30 @@ def get_surrounding_plants(plant_type):
   islands = {}
   for index_1, loop_element_1 in enumerate(indicies):
     for index_2, loop_element_2 in enumerate(indicies):
-      if loop_element_1 == loop_element_2:
+      if index_1 == index_2:
         continue
       
       if (abs(loop_element_1[0] - loop_element_2[0]) > 1 or
           abs(loop_element_1[1] - loop_element_2[1]) > 1):
         continue
 
-      if index_2 == len(data[0]) - 1 or index_1 == len(data) - 1:
-        continue
+      if index_1 == len(data) - 1 or index_2 == len(data[0]) - 1 :
+        break
 
       # Check if island already exists
-      print(islands.get(data[index_1][index_2]))
-      if islands.get(data[index_1][index_2]) is None:
+      print(index_1, index_2, "Touching")
+      print(loop_element_1, loop_element_2)
+      try:
+        islands[data[index_1][index_2]] = [[islands[data[index_1][index_2]]], (index_1, loop_element_1[0]), (index_2, loop_element_2[0])]
+      except KeyError:
         islands[data[index_1][index_2]] = [(index_1, loop_element_1[0]), (index_2, loop_element_2[0])]
-      else:
-        islands[data[index_1][index_2]] = [(index_1, loop_element_1[0]), (index_2, loop_element_2[0])]
+      print(islands)
+
+      # print(islands.get(data[index_1][index_2], None))
+      # if islands.get(data[index_1][index_2], None) is None:
+      #   islands[data[index_1][index_2]] = [(index_1, loop_element_1[0]), (index_2, loop_element_2[0])]
+      # else:
+      #   islands[data[index_1][index_2]] = [(index_1, loop_element_1[0]), (index_2, loop_element_2[0])]
 
 print(data)
 get_surrounding_plants("B")
